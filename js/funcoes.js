afffee6f582c7fc66987a777783f6159f23de012
LuanPeixotop <luanpeixotopereira@gmail.com>
2017-07-30 21:00:43 -0300

    Primeira versão

---
+++ js/funcoes.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.horarios').hide();
+ });
+
+function horarios() {
+    $('.horarios').show();
+}
+
+function alertar() {
+    alert("Parabéns! Sua disciplina foi agendada e sua alma agora pertence ao lorde das trevas!");
+}
+
+function alertarcancelar() {
+    alert("Disciplina cancelada com sucesso!");
+}
